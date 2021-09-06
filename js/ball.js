c76c24663195cb082313e366f93162bdc16a260c
Maxim Petrischak <petrischak.maxim@gmail.com>
2021-09-06 18:09:00 +0300

    feat: code formatting

--- js/ball.js
+++ js/ball.js
@@ -1,91 +1,83 @@
-function Ball(x, y) {
-  this.velocity = 6;
-  this.dx = 0;
-  this.dy = 0;
-  this.x = 440;
-  this.y = 440;
-  this.width = 40;
-  this.height = 40;
+function Ball() {
+    this.velocity = 6;
+    this.dx = 0;
+    this.dy = 0;
+    this.x = 440;
+    this.y = 440;
+    this.width = 40;
+    this.height = 40;
 
-  this.start = function () {
-    this.dy = -this.velocity;
-    this.dx = this.getRandomDirection(-this.velocity, this.velocity);
-  };
+    this.start = function () {
+        this.dy = -this.velocity;
+        this.dx = this.getRandomDirection(-this.velocity, this.velocity);
+    };
 
-  this.fly = function () {
-    if (this.dy) {
-      this.y += this.dy;
-    }
-    if (this.dx) {
-      this.x += this.dx;
-    }
-  };
+    this.fly = function () {
+        if (this.dy) {
+            this.y += this.dy;
+        }
+        if (this.dx) {
+            this.x += this.dx;
+        }
+    };
 
-  this.collide = function (element) {
-    var x = this.x + this.dx;
-    var y = this.y + this.dy;
+    this.isCollision = function (element) {
+        var x = this.x + this.dx;
+        var y = this.y + this.dy;
 
-    if (
-      x + this.width > element.x && // LEFT
-      x < element.x + element.width && //RIGHT
-      y + this.height > element.y && // BOTTOM
-      y < element.y + element.height // TOP
-    ) {
-      return true;
-    }
-    return false;
-  };
+        return (
+            x + this.width > element.x && // LEFT
+            x < element.x + element.width && //RIGHT
+            y + this.height > element.y && // BOTTOM
+            y < element.y + element.height // TOP
+        );
+    };
 
-  this.bumpBlock = function (block) {
-    this.dy *= -1;
-    this.destroyBlock(block);
-  };
+    this.bumpBlock = function () {
+        this.dy *= -1;
+    };
 
-  this.destroyBlock = function (block) {
-    block.active = false;
-  };
+    this.bumpPlatform = function (platform) {
+        if (platform.dx) {
+            this.x += platform.dx;
+        }
 
-  this.bumpPlatform = function (platform) {
-    if (platform.dx) {
-      this.x += platform.dx;
-    }
+        if (this.dy > 0) {
+            this.dy = -this.velocity;
+            var touchX = this.x + this.width / 2;
+            this.dx = this.velocity * platform.getTouchOffset(touchX);
+        }
+    };
 
-    if (this.dy > 0) {
-      this.dy = -this.velocity;
-      var touchX = this.x + this.width / 2;
-      this.dx = this.velocity * platform.getTouchOffset(touchX);
-    }
-  };
+    this.collideWorldBounds = function (callback) {
+        var x = this.x + this.dx;
+        var y = this.y + this.dy;
 
-  this.collideWorldBounds = function (callback) {
-    var x = this.x + this.dx;
-    var y = this.y + this.dy;
+        var ballLeft = x;
+        var ballRight = ballLeft + this.width;
+        var ballTop = y;
+        var ballBottom = ballTop + this.height;
 
-    var ballLeft = x;
-    var ballRight = ballLeft + this.width;
-    var ballTop = y;
-    var ballBottom = ballTop + this.height;
+        var worldLeft = 0;
+        var worldRight = game.width;
+        var worldTop = 0;
+        var worldBottom = game.height;
 
-    var worldLeft = 0;
-    var worldRight = game.width;
-    var worldTop = 0;
-    var worldBottom = game.height;
+        if (ballLeft < worldLeft) {
+            this.x = 0;
+            this.dx = this.velocity;
+        } else if (ballRight > worldRight) {
+            this.x = worldRight - this.width;
+            this.dx = -this.velocity;
+        } else if (ballTop < worldTop) {
+            this.y = 0;
+            this.dy = this.velocity;
+        } else if (ballBottom > worldBottom) {
+            callback();
+        }
+    };
 
-    if (ballLeft < worldLeft) {
-      this.x = 0;
-      this.dx = this.velocity;
-    } else if (ballRight > worldRight) {
-      this.x = worldRight - this.width;
-      this.dx = -this.velocity;
-    } else if (ballTop < worldTop) {
-      this.y = 0;
-      this.dy = this.velocity;
-    } else if (ballBottom > worldBottom) {
-      callback();
-    }
-  };
-
-  this.getRandomDirection = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+    this.getRandomDirection = function (min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    };
 }
--- js/platform.js
+++ js/platform.js
@@ -1,55 +1,55 @@
 function Platform(ball) {
-  this.x = 340;
-  this.y = 480;
-  this.width = 251;
-  this.height = 41;
-  this.velocity = 12;
-  this.dx = 0;
-  this.ball = ball;
-
-  this.moveLeft = function () {
-    this.dx = -this.velocity;
-  };
-
-  this.moveRight = function () {
-    this.dx = this.velocity;
-  };
-
-  this.stop = function () {
+    this.x = 340;
+    this.y = 480;
+    this.width = 251;
+    this.height = 41;
+    this.velocity = 12;
     this.dx = 0;
-  };
+    this.ball = ball;
 
-  this.move = function () {
-    if (this.dx) {
-      this.x += this.dx;
-      if (this.ball) {
-        this.ball.x += this.dx;
-      }
-    }
-  };
+    this.moveLeft = function () {
+        this.dx = -this.velocity;
+    };
 
-  this.fire = function () {
-    if (this.ball) {
-      this.ball.start();
-      this.ball = null;
-    }
-  };
+    this.moveRight = function () {
+        this.dx = this.velocity;
+    };
 
-  this.getTouchOffset = function (x) {
-    var diff = this.x + this.width - x; // RIGHT
-    var offset = this.width - diff; // LEFT
-    var result = (2 * offset) / this.width;
-    return result - 1;
-  };
+    this.stop = function () {
+        this.dx = 0;
+    };
 
-  this.collideWorldBounds = function (width) {
-    var platformLeft = this.x + this.dx;
-    var platformRight = platformLeft + this.width;
-    var worldLeft = 0;
-    var worldRight = width;
+    this.move = function () {
+        if (this.dx) {
+            this.x += this.dx;
+            if (this.ball) {
+                this.ball.x += this.dx;
+            }
+        }
+    };
 
-    if (platformLeft < worldLeft || platformRight > worldRight) {
-      this.dx = 0;
-    }
-  };
+    this.fire = function () {
+        if (this.ball) {
+            this.ball.start();
+            this.ball = null;
+        }
+    };
+
+    this.getTouchOffset = function (x) {
+        var diff = this.x + this.width - x; // RIGHT
+        var offset = this.width - diff; // LEFT
+        var result = (2 * offset) / this.width;
+        return result - 1;
+    };
+
+    this.collideWorldBounds = function (width) {
+        var platformLeft = this.x + this.dx;
+        var platformRight = platformLeft + this.width;
+        var worldLeft = 0;
+        var worldRight = width;
+
+        if (platformLeft < worldLeft || platformRight > worldRight) {
+            this.dx = 0;
+        }
+    };
 }
