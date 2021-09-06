94a86547fef7f80223e0c6029e8ce42c06f14817
Maxim Petrischak <petrischak.maxim@gmail.com>
2021-09-06 18:05:55 +0300

    feat: add destroy method

--- js/block.js
+++ js/block.js
@@ -1,8 +1,13 @@
 function Block(col, row) {
-  this.active = true;
-  this.width = 111;
-  this.height = 39;
-  this.offset = 5;
-  this.x = (this.width + this.offset) * col + this.width;
-  this.y = (this.height + this.offset) * row + this.height;
+    var me = this;
+    this.active = true;
+    this.width = 111;
+    this.height = 39;
+    this.offset = 5;
+    this.x = (this.width + this.offset) * col + this.width;
+    this.y = (this.height + this.offset) * row + this.height;
+
+    this.destroy = function () {
+        me.active = false;
+    };
 }
